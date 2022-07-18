9827ead0e6072f0dd615b5d8b0f67bc4ab97fac4
kila.liao <kila.liao@104.com.tw>
2022-07-18 10:16:02 +0800

    0

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World! version1"', () => {
-      expect(appController.getHello()).toBe('Hello World! version1');
+      expect(appController.getHello()).toBe('Hello World! version2');
     });
   });
 });
