85462990a18f8678ebf882f10914b2d78ab5e622
kila.liao <kila.liao@104.com.tw>
2022-07-22 16:17:14 +0800

    d

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
-  @Get()
+  @Get('prod')
   getHello(): string {
     console.log('health check');
     return this.appService.getHello();
