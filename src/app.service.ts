fc067cd2278d22efcb27e235d3fbcfcb64531d8b
kila.liao <kila.liao@104.com.tw>
2022-07-22 18:27:22 +0800

    s

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! version9';
+    return 'Hello World! version10';
   }
 }
