6e41a4777b8a16a3255bfb94e2772af0f2219af5
kila.liao <kila.liao@104.com.tw>
2022-07-25 11:36:37 +0800

    ss

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -20,7 +20,7 @@ export class AppController {
   @Get('prod/test')
   getHelloProdTest(): string {
     console.log('prod test');
-    return 'I am prod test4';
+    return 'I am prod test5';
   }
 
   @Get('dev')
@@ -32,7 +32,7 @@ export class AppController {
   @Get('dev/test')
   getHelloDevTest(): string {
     console.log('dev test');
-    return 'I am dev test4';
+    return 'I am dev test5';
   }
 
   @Get('dead')
